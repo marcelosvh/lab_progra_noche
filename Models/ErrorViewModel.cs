b677247120248ab489af2bb9f069ecf8ffba897b
Marcelo <marcelosvh@hotmail.com>
2018-09-22 18:40:29 -0500

    Creando proyecto lab progra

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAB_PROGRA_NOCHE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
